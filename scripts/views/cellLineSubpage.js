2c6bc8bd2fbe7f26b9f7145eb56e54b9828dd8de
meganrm <meganrm@gmail.com>
2016-10-19 16:52:00 -0700

    set up content placeholders

--- scripts/views/cellLineSubpage.js
+++ scripts/views/cellLineSubpage.js
@@ -25,6 +25,11 @@
     if ($('#cellline-info').find('ul').length ===0){
       $('#cellline-info').append(cellLine.toHtml($('#cell-profile-info-template')));
       $('#editingdesign').append(cellLine.toHtml($('#editingDesign-template')));
+      $('#genomiccharacterizations').append(cellLine.toHtml($('#genomiccharacterizations-template')));
+      $('#validation').append(cellLine.toHtml($('#validation-template')));
+      $('#stemcellcharacteristics').append(cellLine.toHtml($('#stemcellcharacteristics-template')));
+      $('#supplementaryinformation').append(cellLine.toHtml($('#supplementaryinformation-template')));
+
       cellLineProfileView.handleSubNav();
       cellLineProfileView.goBack();
     }
