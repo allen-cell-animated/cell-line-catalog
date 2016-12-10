714e5eedc20a28e5ef880fb79d7e5eb844b7085b
Megan Riel-Mehan <meganrm@gmail.com>
2016-12-09 16:28:33 -0800

    disabled links

--- scripts/views/cellLineSubpage.js
+++ scripts/views/cellLineSubpage.js
@@ -3,6 +3,26 @@
 
   var cellLineProfileView = {};
 
+  cellLineProfileView.disableLinks = function(){
+    $('.subpage-nav').on('click', '.disabled', function(e){
+      e.preventDefault();
+      console.log('disabled');
+      return false;
+    })
+  }
+
+  cellLineProfileView.checkforData = function(cellLine){
+    cellLine.subpaged_status.forEach(function(a){
+      if (a.done==='true') {
+        $('#tab_' + a.id).removeClass('disabled');
+        if (a.id === 'editingdesign') {
+          $('#tab_' + a.id).addClass('active');
+          $('#' + a.id).addClass('active');
+
+        }
+      }
+    })
+  };
 
   cellLineProfileView.gallery = function() {
     $('.thumbnails').on('click', '.morphology-thumbnail', function(e) {
@@ -24,25 +44,31 @@
       $('#cellline-info').append(cellLine.toHtml($('#cell-profile-info-template')));
       $('#editingdesign').append(cellLine.toHtml($('#editingDesign-template')));
       $('#genomiccharacterizations').append(cellLine.toHtml($('#genomiccharacterizations-template')));
-      cellLine['GenomicCharacterization_ddpcr'].forEach(function(a){
-        $('#ddPCR-table').append(cellLine.nestedToHtml($('#ddPCR-template'), a ));
-      });
-      cellLine['GenomicCharacterization_offtargets'].forEach(function(a){
-        $('#off-target-table').append(cellLine.nestedToHtml($('#offTargets-template'), a ));
-      });
-
-
+      if (typeof(cellLine['GenomicCharacterization_ddpcr']) !=='string') {
+        cellLine['GenomicCharacterization_ddpcr'].forEach(function(a){
+          $('#ddPCR-table').append(cellLine.nestedToHtml($('#ddPCR-template'), a ));
+        });
+      }
+      if (typeof(cellLine['GenomicCharacterization_offtargets']) !=='string') {
+        cellLine['GenomicCharacterization_offtargets'].forEach(function(a){
+          $('#off-target-table').append(cellLine.nestedToHtml($('#offTargets-template'), a ));
+        });
+      }
       $('#validation').append(cellLine.toHtml($('#validation-template')));
       $('#stemcellcharacteristics').append(cellLine.toHtml($('#stemcellcharacteristics-template')));
-      cellLine['StemCellCharacterization_pluripotency_analysis'].forEach(function(a){
-        $('#pluripotent-table-body').append(cellLine.nestedToHtml($('#pluripotent-template'), a ));
-      });
-
+      if (typeof(cellLine['StemCellCharacterization_pluripotency_analysis'])!=='string') {
+        cellLine['StemCellCharacterization_pluripotency_analysis'].forEach(function(a){
+          $('#pluripotent-table-body').append(cellLine.nestedToHtml($('#pluripotent-template'), a ));
+        });
+      }
       $('#supplementaryinformation').append(cellLine.toHtml($('#supplementaryinformation-template')));
       // cellLineProfileView.goBack();
       cellLineProfileView.gallery();
       $('#cell-collection-banner').show();
+      cellLineProfileView.checkforData(cellLine);
+      cellLineProfileView.disableLinks();
       $('#cellline-profile').show();
+
     }
   };
 
