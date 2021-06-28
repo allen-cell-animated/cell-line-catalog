c024a93de09f962e6dcac29f1b9c11b5d65069cd
Zizhen Song <zizhensong@gmail.com>
2021-06-28 08:47:38 -0700

    finished

--- scripts/models/cellLine.js
+++ scripts/models/cellLine.js
@@ -18,15 +18,61 @@
   CellLine.allCellLinesFB = [];
 
   CellLine.prototype.toHtml= function(templateid){
-    var source = $(templateid).html();
-    var renderTemplate = Handlebars.compile(source);
-    return renderTemplate(this);
+    return CellLine.prototype.nestedToHtml(templateid, this);
   };
 
   CellLine.prototype.nestedToHtml= function(templateid, nestedObj){
     var source = $(templateid).html();
     var renderTemplate = Handlebars.compile(source);
-    return renderTemplate(nestedObj);
+    var html = renderTemplate(nestedObj);
+
+    // Using template to covert html to js element
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    finalElement = template.content.firstChild;
+
+    // Populate Gene Symbol Text entry
+    if (finalElement instanceof Element) {
+      var isoformList = finalElement.querySelector('#editingDesign-info');
+      var index = 0;
+      if (isoformList && nestedObj.Main_isoforms) {
+        nestedObj.Main_isoforms.forEach(element => {
+          var subList = document.createElement('span');
+          subList.classList.add('list-group-item');
+          // KEY
+          var childkey = document.createElement('span');
+          childkey.classList.add('profile-summary-key');
+          childkey.textContent = element.gene_name + " NCBI Isoform(s):";
+          subList.append(childkey);
+
+          // ISOFORM LINKS
+          if (element.gene_isoforms)
+            for (var i = 0; i < element.gene_isoforms.length; i++) {
+              // populate link for one isoform
+              var isoform = element.gene_isoforms[i];
+              var child = document.createElement('span');
+              var link = document.createElement('a');
+              child.classList.add('profile-summary-value');
+              link.href = 'http://www.ncbi.nlm.nih.gov/nuccore/' + isoform;
+              link.textContent = isoform;
+              child.appendChild(link);
+              subList.append(child);
+
+              // if need divider
+              if (i < element.gene_isoforms.length -1){
+                var slash= document.createElement('span');
+                slash.textContent = ' / '
+                subList.append(slash);
+              }
+            };
+          isoformList.insertBefore(subList, isoformList.children[index]);
+          index++;
+        });
+      }
+
+    }
+    return finalElement;
   };
 
   CellLine.loadIntoObjectArray = function(name){
